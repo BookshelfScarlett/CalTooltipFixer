3d54f07ae9579c10218d9cc79cf48e101f47de4b
Scarlet <143493496+BookshelfScarlett@users.noreply.github.com>
2025-08-23 11:19:40 +0800

    星辉套数值相关

--- Content/Armor/FixedAstralArmor.cs
+++ Content/Armor/FixedAstralArmor.cs
@@ -4,6 +4,10 @@ namespace CalTooltipFixer.Content.Armor
 {
     public class FixedAstralArmor : BaseFixedArmor
     {
+        #region 数值
+        public const int FallenStarDamage = 120;
+        public const double HitDamageReduction = 6.25
+        #endregion
         public override int HeadType => ArmorType<AstralHelm>();
         public override int BodyType => ArmorType<AstralBreastplate>();
         public override int LegsType => ArmorType<AstralLeggings>();
